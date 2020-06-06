efca289dde73f5a9fa7b2190cd4bfce54bace255
aNSHAJ <anshajkumarsharma@gmail.com>
2020-06-06 16:10:53 +0530

    Network identification done

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+//-------------------------------------
+// Responsible for setting up the player.
+// This includes adding/removing him correctly on the network.
+//-------------------------------------
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsTodisable;
+
+    Camera sceneCamera ;
+    // Disable components that should only be
+	// active on the player that we control
+    void Start()
+    {
+        sceneCamera = Camera.main;
+
+        if(!isLocalPlayer)
+        {
+            for( int i=0;i<componentsTodisable.Length;i++)
+            {
+                componentsTodisable[i].enabled = false;
+            }
+        }else{
+            // We are the local player: Disable the scene camera
+            if(sceneCamera != null)
+            {
+               sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    } 
+    // When we are destroyed
+    void OnDisable()
+    {
+        // Re-enable the scene camera
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# MULTIPLAYER FPS GAME
+
+It is a simple multiplayer first person shooting game in C#(C Sharp) using Unity3D
+
+
